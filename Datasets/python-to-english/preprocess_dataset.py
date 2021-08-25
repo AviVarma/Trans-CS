12aba7be8145479e96f9938f7c4d6d2b4f40e2f4
Avi Varma <avi.varma@ymail.com>
2021-08-25 21:34:48 +0100

    Create pre-processor script for english-to-python dataset

---
+++ Datasets/python-to-english/preprocess_dataset.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import json
+import os
+import torch
+import argparse
+
+def loadDataset(filepath):
+    file = open(filepath, "r", encoding="utf-8")
+    fileLines = f.readlines()
+
+
+
+if __name__ == '__main__':
+    main()
