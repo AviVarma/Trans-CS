8d046e1e932521daa90817577a00c6a81dab05f1
Avi Varma <avi.varma@ymail.com>
2021-08-26 02:25:30 +0100

    Added gpu support.

--- Components/enviroment_variables.py
+++ Components/enviroment_variables.py
@@ -1,6 +1,9 @@
+import torch
+
 SEED = 1234
 INIT_TOKEN = '<sos>'
 EOS_TOKEN = '<eos>'
 TOKENIZER = 'spacy'
 MIN_FREQ = 0
 DATASET_PATH = '../Datasets/english_python_data.txt'
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
