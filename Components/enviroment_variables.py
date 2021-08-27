7f1b931c3fc42d38ca760cd74388c158e6bbb961
Avi Varma <avi.varma@ymail.com>
2021-08-27 21:14:40 +0100

    env vars from train file added.

--- Components/enviroment_variables.py
+++ Components/enviroment_variables.py
@@ -11,6 +11,7 @@ VOCAB_INPUT = "../Vocabulary/Input_vocab.pkl"
 VOCAB_OUTPUT = "../Vocabulary/Output_vocab.pkl"
 TRAIN_DF_PATH = "../Datasets/english_python_dataframe/english_python_train.json"
 VAL_DF_PATH = "../Datasets/english_python_dataframe/english_python_val.json"
+MODEL_SAVE_PATH = "../Checkpoints/model.pt"
 
 # vars required for training
 #INPUT_DIM = len(Input.vocab)
@@ -24,6 +25,7 @@ ENC_PF_DIM = 512
 DEC_PF_DIM = 512
 ENC_DROPOUT = 0.1
 DEC_DROPOUT = 0.1
+BATCH_SIZE = 16
 LEARNING_RATE = 0.0005
 N_EPOCHS = 100
 CLIP = 1
