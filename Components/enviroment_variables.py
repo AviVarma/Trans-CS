40d313a52bb3a15ef50b054d896206d0ad9f3ea1
Avi Varma <avi.varma@ymail.com>
2021-08-30 00:17:37 +0100

    Added back the necessary hyper-parameters for training.

--- Components/enviroment_variables.py
+++ Components/enviroment_variables.py
@@ -14,3 +14,17 @@ VOCAB_OUTPUT = "Vocabulary/Output.pkl"
 TRAIN_DF_PATH = "Datasets/english_python_dataframe/english_python_train.json"
 VAL_DF_PATH = "Datasets/english_python_dataframe/english_python_val.json"
 MODEL_SAVE_PATH = "Checkpoints/model.pt"
+
+HID_DIM = 256
+ENC_LAYERS = 3
+DEC_LAYERS = 3
+ENC_HEADS = 16
+DEC_HEADS = 16
+ENC_PF_DIM = 512
+DEC_PF_DIM = 512
+ENC_DROPOUT = 0.1
+DEC_DROPOUT = 0.1
+BATCH_SIZE = 16
+LEARNING_RATE = 0.0005
+N_EPOCHS = 100
+CLIP = 1
