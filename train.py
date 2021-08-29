acc6f83fe9d7957149376b7a51d704dc24366c05
Avi Varma <avi.varma@ymail.com>
2021-08-29 01:07:00 +0100

    Moved make_trg_mask to train as it's only used in training.

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import torch.optim
 from Model.Models import Encoder, Decoder, Seq2Seq
 from Components import enviroment_variables as env
-from Components.utils import load, save, make_trg_mask
+from Components.utils import load, save
 from torchtext.legacy.data import Field, BucketIterator, Iterator
 from torchtext.legacy import data
 import torch.nn as nn
@@ -18,6 +18,26 @@ def count_parameters(model):
 def initialize_weights(w):
     if hasattr(w, 'weight') and w.weight.dim() > 1:
         nn.init.xavier_uniform_(w.weight.data)
+
+
+def make_trg_mask(trg):
+    # trg = [batch size, trg len]
+
+    trg_pad_mask = (trg != env.TRG_PAD_IDX).unsqueeze(1).unsqueeze(2)
+
+    # trg_pad_mask = [batch size, 1, 1, trg len]
+
+    trg_len = trg.shape[1]
+
+    trg_sub_mask = torch.tril(torch.ones((trg_len, trg_len), device=env.DEVICE)).bool()
+
+    # trg_sub_mask = [trg len, trg len]
+
+    trg_mask = trg_pad_mask & trg_sub_mask
+
+    # trg_mask = [batch size, 1, trg len, trg len]
+
+    return trg_mask
 
 
 def train(model, iterator, optimizer, criterion, clip):
@@ -106,24 +126,24 @@ def main():
     val_df = load(env.VAL_DF_PATH, dataframe=True)
 
     # load vocabularies
-    Input = load(env.VOCAB_INPUT)  # try adding these to environment vars.
-    Output = load(env.VOCAB_OUTPUT)
+    # Input = load(env.VOCAB_INPUT)  # try adding these to environment vars.
+    # Output = load(env.VOCAB_OUTPUT)
+    #
+    # fields = [('Input', Input), ('Output', Output)]
+    #
+    # INPUT_DIM = len(Input.vocab)
+    # OUTPUT_DIM = len(Output.vocab)
 
-    fields = [('Input', Input), ('Output', Output)]
-
-    INPUT_DIM = len(Input.vocab)
-    OUTPUT_DIM = len(Output.vocab)
-
-    enc = Encoder(INPUT_DIM, env.HID_DIM, env.ENC_LAYERS, env.ENC_HEADS,
+    enc = Encoder(env.INPUT_DIM, env.HID_DIM, env.ENC_LAYERS, env.ENC_HEADS,
                   env.ENC_PF_DIM, env.ENC_DROPOUT, env.DEVICE)
 
-    dec = Decoder(OUTPUT_DIM, env.HID_DIM, env.DEC_LAYERS, env.DEC_HEADS,
+    dec = Decoder(env.OUTPUT_DIM, env.HID_DIM, env.DEC_LAYERS, env.DEC_HEADS,
                   env.DEC_PF_DIM, env.DEC_DROPOUT, env.DEVICE)
 
-    SRC_PAD_IDX = Input.vocab.stoi[Input.pad_token]
-    TRG_PAD_IDX = Output.vocab.stoi[Output.pad_token]
+    # SRC_PAD_IDX = Input.vocab.stoi[Input.pad_token]
+    # TRG_PAD_IDX = Output.vocab.stoi[Output.pad_token]
 
-    model = Seq2Seq(enc, dec, SRC_PAD_IDX, TRG_PAD_IDX, env.DEVICE).to(env.DEVICE)
+    model = Seq2Seq(enc, dec, env.SRC_PAD_IDX, env.TRG_PAD_IDX, env.DEVICE).to(env.DEVICE)
     model.apply(initialize_weights)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=env.LEARNING_RATE)
@@ -141,21 +161,20 @@ def main():
 
         for i in range(train_df.shape[0]):
             try:
-                ex = data.Example.fromlist([train_df.question[i], train_df.solution[i]], fields)
+                ex = data.Example.fromlist([train_df.question[i], train_df.solution[i]], env.fields)
                 train_example.append(ex)
             except:
                 pass
 
         for i in range(val_df.shape[0]):
             try:
-                ex = data.Example.fromlist([val_df.question[i], val_df.solution[i]], fields)
+                ex = data.Example.fromlist([val_df.question[i], val_df.solution[i]], env.fields)
                 val_example.append(ex)
             except:
                 pass
 
-        train_data = data.Dataset(train_example, fields)
-        valid_data = data.Dataset(val_example, fields)
-
+        train_data = data.Dataset(train_example, env.fields)
+        valid_data = data.Dataset(val_example, env.fields)
 
         train_iterator, valid_iterator = BucketIterator.splits((train_data, valid_data), batch_size=env.BATCH_SIZE,
                                                                sort_key=lambda x: len(x.Input),
@@ -171,7 +190,7 @@ def main():
         if valid_loss < best_valid_loss:
             best_valid_loss = valid_loss
             save(env.MODEL_SAVE_PATH, model=model)
-            #torch.save(model.state_dict(), '/content/drive/MyDrive/TheSchoolOfAI/EndCapstone/model.pt')
+            # torch.save(model.state_dict(), '/content/drive/MyDrive/TheSchoolOfAI/EndCapstone/model.pt')
 
         print(f'Epoch: {epoch + 1:02} | Time: {epoch_mins}m {epoch_secs}s')
         print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
