8ba47caa0a3b10cd577f9c8a41d05ddbc7a4586c
Avi Varma <avi.varma@ymail.com>
2021-09-03 00:13:13 +0100

    Added PEP8 compliant comments for all functions.

--- Components/utils.py
+++ Components/utils.py
@@ -9,7 +9,7 @@ from torchtext.legacy import data
 
 def save(filename, dataframe: pd.DataFrame = None, vocab: data = None, model=None):
     """
-    function will save both modified datasets and models
+    Save both modified datasets and models
     param filename: the file path eg: ./here/file.json
     param dataframe: default is none, provide a dataframe to save.
     param model: default is none, provide a model to save.
@@ -29,6 +29,15 @@ def save(filename, dataframe: pd.DataFrame = None, vocab: data = None, model=Non
 
 
 def load(filename, dataframe=False, vocab=False, model=False):
+    """
+    Load datasets byt providing the file path and setting dataframe to true.
+    :param filename: the file path eg: ./here/file.json
+    :param dataframe: Boolean, if you want to load a dataframe.
+    :param vocab: Boolean, if you want to load a vocabulary.
+    :param model: Boolean, if you want to load a model.
+    :return: Respective data from the path provided.
+    """
+
     if dataframe:
         return pd.read_json(filename)
     if vocab:
@@ -36,20 +45,28 @@ def load(filename, dataframe=False, vocab=False, model=False):
 
 
 def make_trg_mask(trg, TRG_PAD_IDX):
-    # trg = [batch size, trg len]
-
-    trg_pad_mask = (trg != TRG_PAD_IDX).unsqueeze(1).unsqueeze(2)
+    """
+    Create a mask for the <pad> tokens like in source mask.
+    Then create a subsequent mask which is a diagonal matrix where:
+    1. the elements above the diagonal will be zero.
+    2. the elements below the diagonal will be set to the value in the input tensor.
+    The subsequent mask is now concatenated with the padding mask using "AND" operator to combine
+    the two masks ensuring both the subsequent tokens and the padding tokens cannot be attended to.
+    :param trg: Target sequence. [batch size, trg len]
+    :param TRG_PAD_IDX: Target sequence tokenized and changed to integers for mask creation.
+    (elements below the diagonal matrix will be set to the value in the input tensor).
+    :return trg_mask: Target mask. [batch size, 1, trg len, trg len]
+    """
 
     # trg_pad_mask = [batch size, 1, 1, trg len]
+    trg_pad_mask = (trg != TRG_PAD_IDX).unsqueeze(1).unsqueeze(2)
 
     trg_len = trg.shape[1]
 
+    # trg_sub_mask = [trg len, trg len]
     trg_sub_mask = torch.tril(torch.ones((trg_len, trg_len), device=env.DEVICE)).bool()
 
-    # trg_sub_mask = [trg len, trg len]
-
-    trg_mask = trg_pad_mask & trg_sub_mask
-
     # trg_mask = [batch size, 1, trg len, trg len]
+    trg_mask = trg_pad_mask & trg_sub_mask
 
     return trg_mask
