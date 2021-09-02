bac3d8119d6505d9f225559573d211a64923e40e
Avi Varma <avi.varma@ymail.com>
2021-09-02 11:26:54 +0100

    Added documentation scaffolding.

--- Model/Models.py
+++ Model/Models.py
@@ -6,14 +6,33 @@ import numpy as np
 
 class Encoder(nn.Module):
     """
-    Encoder module from 'Attention is all you need'
+    Encoder module from 'BERT (using positional embeddings)'
     """
     def __init__(self, input_dim, hid_dim, n_layers, n_heads, pf_dim, dropout, device, max_length=1000):
+        """
+        Initialize the tokens through an embedding layer.
+        Now the order of tokens must be recorded using the positional embedding layer (pos_embedding).
+        Initialize the Encoder sub layers.
+        Initialize the dropout and scale function.
+        :param input_dim: size of input vocabulary, (n_src_vocab from original transformer).
+        :param hid_dim: Output dimension for encoder (d_model from original transformer).
+        :param n_layers: number of identical stack layers.
+        :param n_heads: number of heads in multi-head attention.
+        :param pf_dim: Position-wise Feedforward Layer dimension, usually larger than hid_dim.
+        :param dropout: nn.Dropout()
+        :param device: run model on GPU or CPU.
+        :param max_length: maximum length of sequence default 1000
+        """
+
         super().__init__()
 
         self.device = device
 
         self.tok_embedding = nn.Embedding(input_dim, hid_dim)
+
+        # positional embedding: position of the token within the sequence, starting with
+        # the first token, (<sos>) in position 0. The position embedding has a "vocabulary"
+        # size of 100, which means our model can accept sentences up to 100 tokens long.
         self.pos_embedding = nn.Embedding(max_length, hid_dim)
 
         self.layers = nn.ModuleList([EncoderLayer(hid_dim, n_heads, pf_dim, dropout, device)
@@ -25,32 +44,55 @@ class Encoder(nn.Module):
 
     def forward(self, src, src_mask):
         """
+        Used in the encoder layers to mask the multi-head attention mechanisms.
+        This is to calculate and apply attention over the input query.
+        the model does not pay attention to <pad>.
 
-        :param src: [batch size, src len]
-        :param src_mask: [batch size, 1, 1, src len]
-        :return src: [batch size, src len, hid dim]
+        The tok_embedding and pos_embedding are summed together elementwise which returns a vector
+        containing information about the token and it's position within the sentence.
+        The token embeddings are multiplied by scaling factor (hid_dim) to reduce variance in
+        embeddings. Dropout is applied to the combined embeddings.
+        The combined embeddings are then passed through N encoder layers to get src:
+        [batch size, src len, hid dim]
+        :param src: the source sentence. [batch size, src len]
+        :param src_mask: source mask: same shape as source sentence, but 1 when
+         the token in src sentence is not <pad>, else, 0. [batch size, 1, 1, src len]
+        :return src: combined embeddings are then passed through N encoder layers.
+        [batch size, src len, hid dim]
         """
 
         batch_size = src.shape[0]
         src_len = src.shape[1]
 
+        # pos = [batch size, src len]
         pos = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
 
-        # pos = [batch size, src len]
-        src = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
-
         # src = [batch size, src len, hid dim]
+        src = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
 
         for layer in self.layers:
             src = layer(src, src_mask)
 
         # src = [batch size, src len, hid dim]
-
         return src
 
 
 class Decoder(nn.Module):
     def __init__(self, output_dim, hid_dim, n_layers, n_heads, pf_dim, dropout, device, max_length=10000):
+        """
+        Similar to encoder, the tokens and positional embedding layers are initialized.
+        The decoder layer is initialized.
+        The decoder representation after the Nth layer is then passed through a linear layer (fc_out)
+        The softmax operation is contained within the loss function.
+        :param output_dim: size of output vocabulary, (n_trg_vocab from original transformer).
+        :param hid_dim: Output dimension for decoder (d_model from original transformer).
+        :param n_layers: number of identical stack layers.
+        :param n_heads: number of heads in multi-head attention.
+        :param pf_dim: Position-wise Feedforward Layer dimension, usually larger than hid_dim.
+        :param dropout: nn.Dropout()
+        :param device: run model on GPU or CPU.
+        :param max_length: maximum length of source code string default 10000
+        """
         super().__init__()
 
         self.device = device
@@ -67,6 +109,14 @@ class Decoder(nn.Module):
         self.scale = torch.sqrt(torch.FloatTensor([hid_dim])).to(device)
 
     def forward(self, trg, enc_src, trg_mask, src_mask):
+        """
+
+        :param trg:
+        :param enc_src:
+        :param trg_mask:
+        :param src_mask:
+        :return:
+        """
         # trg = [batch size, trg len]
         # enc_src = [batch size, src len, hid dim]
         # trg_mask = [batch size, 1, trg len, trg len]
@@ -97,7 +147,16 @@ class Decoder(nn.Module):
 
 
 class Seq2Seq(nn.Module):
+
     def __init__(self, encoder, decoder, src_pad_idx, trg_pad_idx, device):
+        """
+
+        :param encoder:
+        :param decoder:
+        :param src_pad_idx:
+        :param trg_pad_idx:
+        :param device:
+        """
         super().__init__()
 
         self.encoder = encoder
@@ -107,15 +166,22 @@ class Seq2Seq(nn.Module):
         self.device = device
 
     def make_src_mask(self, src):
-        # src = [batch size, src len]
-
+        """
+        The source mask is created by checking where the input sequence is not equal to a <pad> token.
+        1 where the token is not a <pad> token, 0 otherwise.
+        Unsqueeze to (1,2) singleton dimension so that energy can be applied on each item within src.
+        :param src: [batch size, src len],
+        :return src_mask: [batch size, n heads, seq len, seq len]
+        """
         src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
-
-        # src_mask = [batch size, 1, 1, src len]
-
         return src_mask
 
     def make_trg_mask(self, trg):
+        """
+
+        :param trg:
+        :return:
+        """
         # trg = [batch size, trg len]
 
         trg_pad_mask = (trg != self.trg_pad_idx).unsqueeze(1).unsqueeze(2)
@@ -135,6 +201,12 @@ class Seq2Seq(nn.Module):
         return trg_mask
 
     def forward(self, src, trg):
+        """
+
+        :param src:
+        :param trg:
+        :return:
+        """
         # src = [batch size, src len]
         # trg = [batch size, trg len]
 
