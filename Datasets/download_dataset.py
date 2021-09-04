ee2819382b08b4e66771a55ee09ee39be1abf1f7
Avi Varma <avi.varma@ymail.com>
2021-09-04 14:41:40 +0100

    Amended download dataset script to download and extract the dataset within the datasets' directory when the script is executed from base directory.

--- Datasets/download_dataset.py
+++ Datasets/download_dataset.py
@@ -4,14 +4,15 @@ import os
 
 def main():
     site_url = "http://www.phontron.com/download/conala-corpus-v1.1.zip"
-    wget.download(site_url)
+    dataset_path = "./Datasets/"
+    wget.download(site_url, dataset_path)
     file = os.path.basename(site_url)
 
-    with ZipFile(file, 'r') as zip:
+    with ZipFile((dataset_path+file), 'r') as zip:
         zip.printdir()
 
         print("extracting...")
-        zip.extractall()
+        zip.extractall(path=dataset_path)
         print("Done!")
 
 
