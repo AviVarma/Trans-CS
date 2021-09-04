47da83573c12e41e7eee1bb88a05b8908bd23424
Avi Varma <avi.varma@ymail.com>
2021-09-04 12:45:44 +0100

    Restructured environment vars to improve readability

--- Components/enviroment_variables.py
+++ Components/enviroment_variables.py
@@ -5,11 +5,18 @@ INIT_TOKEN = '<sos>'
 EOS_TOKEN = '<eos>'
 TOKENIZER = 'spacy'
 MIN_FREQ = 0
-DATASET_PATH = 'Datasets/english_python_data.txt'
+
+#DATASET_PATH = 'Datasets/english_python_data.txt'
+TRAIN_DF_PATH = "Datasets/conala-corpus/conala-train.json"
+VAL_DF_PATH = "Datasets/conala-corpus/conala-test.json"
+
+TRAIN_DF_MODIFIED_PATH = "Datasets/augmented-conala-corpus/conala-train.json"
+VAL_DF_MODIFIED_PATH = "Datasets/augmented-conala-corpus/conala-test.json"
+
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 VOCAB_INPUT = "Saved/Vocabulary/Input.pkl"
 VOCAB_OUTPUT = "Saved/Vocabulary/Output.pkl"
-TRAIN_DF_PATH = "Datasets/english_python_dataframe/english_python_train.json"
-VAL_DF_PATH = "Datasets/english_python_dataframe/english_python_val.json"
+
 MODEL_SAVE_PATH = "Saved/Model_Checkpoints/model.pt"
 ATTENTION_PATH = "Saved/Evaluation_resources/attention_confusion_matrix.png"
